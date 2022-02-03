c3b7863c87a5c926a11716469a9cbee9ea5fbbd6
mathmorais <gm80648@gmail.com>
2022-02-03 19:06:15 -0300

    Add theme

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  white: "#e7e7eb",
+  blue: "#1e213a",
+  darkBlue: "#100e1d",
+  grey: "#6e707a",
+  lightGrey: "#88869d",
+  yellow: "#ffec65",
+};
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     
-:root{
-  --white: #F2F2F2;
-  
-  --gray-100: #e1e1e6;
-  --gray-300: #a8a8b3;
-  --gray-700: #202024;
-  --gray-800: #29292e;
-  --gray-850: #1f2729;
-  --gray-900: #121214;
-  --cyan-500: #61dafb;
-  --cyan-200: #04D9D9; 
-  --yellow-200: #f2aa52;
-  --yellow-500: #eba417;
-  --orange-500: #F26849;
-  --red-200: #f2507b;
-}
-  *{
+*{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -52,7 +36,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body, input, textarea, select, button{
   font: 400 1rem 'Roboto', sans-serif;
-}
+ } 
 
   button {
     cursor: pointer;
@@ -62,4 +46,4 @@ export const GlobalStyle = createGlobalStyle`
     color: inherit;
     text-decoration: none;
   }
-`
+`;
