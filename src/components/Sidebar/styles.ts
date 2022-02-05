36ae0c6654b1123fd244a6717cf562f82101b3d6
Joao Carlos Assis <carlos.rocha.assis@gmail.com>
2022-02-04 23:02:25 -0300

    change style sidebar

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -2,19 +2,20 @@ import styled from "styled-components";
 import { colors } from "../../constants/colors";
 
 export const SidebarContainer = styled.aside`
-  width: 25%;
+  width: 30%;
+  min-width: 300px;
   height: 100vh;
   background: ${colors.blue};
 
   padding: 2rem;
-`
+`;
 
 export const HeaderSidebar = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  svg{
+  
+  svg {
     width: 35px;
     height: 35px;
     color: ${colors.white};
@@ -23,47 +24,53 @@ export const HeaderSidebar = styled.div`
     border-radius: 50%;
     padding: 0.5rem;
   }
-`
+`;
 
 export const ContentSidebar = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin-top: 3rem;
+  margin: auto;
   color: ${colors.lightGrey};
+  gap: 1.5rem;
+  justify-content: space-between;
+  `;
 
-  strong{
-    color: ${colors.white};
-    font-size: 7rem;
-    font-weight: 500;
+  export const MainContentSidebar = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-    span{
-      font-size:2rem;
-      font-weight: 400;
-      color: ${colors.lightGrey};
+    strong {
+      color: ${colors.white};
+      font-size: 7rem;
+      font-weight: 500;
+  
+      span {
+        font-size: 2rem;
+        font-weight: 400;
+        color: ${colors.lightGrey};
+      }
     }
-  }
-
-  p{
-    font-size: 2.25rem;
-    font-weight: 600;
-    padding: 2rem;
-  }
-
-  span{
-    font-size: 1.125rem;
-    font-weight: 500;
-
-  }
-
-`
+  
+    p {
+      font-size: 2.25rem;
+      font-weight: 600;
+      padding: 2rem;
+    }
+  `
 
 export const FooterSidebar = styled.footer`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  margin-top: 1.5rem;
-  svg{
-    font-size: 1.2rem;
-    margin-right: 0.5rem;
+
+  div {
+    margin-top: 1rem;
+    align-items: center;
+    svg {
+      font-size: 1.2rem;
+      margin-right: 0.5rem;
+    }
   }
-`
+`;
