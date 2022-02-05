c25cae9e9cdd06379430f5ae65bb092ad4abc202
mathmorais <gm80648@gmail.com>
2022-02-04 23:09:39 -0300

    Add sidebar to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../Header";
+import { Sidebar } from "../Sidebar";
 import { Container, Main } from "./styles";
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <Container>
+      <Sidebar />
       <Main>
         <Header />
         {children}
