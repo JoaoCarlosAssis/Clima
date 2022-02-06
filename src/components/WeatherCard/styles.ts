2a3d2554025cba71d4c1461959089644b1514cf6
mathmorais <gm80648@gmail.com>
2022-02-05 22:31:17 -0300

    Add WeatherCard component

--- src/components/WeatherCard/styles.ts
+++ src/components/WeatherCard/styles.ts
@@ -5,9 +5,9 @@ export const WeatherCardTemp = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  font-size: 1.2vw;
-  margin-top: 20px;
-
+  font-size: 1.1vw;
+  margin-top: 2px;
+  padding: 0 20px;
   p:last-of-type {
     color: ${colors.gray};
   }
