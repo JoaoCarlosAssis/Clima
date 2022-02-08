005e9fcb0be62a1162afd81e0a34460c37d228c1
mathmorais <gm80648@gmail.com>
2022-02-08 16:13:47 -0300

    Add GeoLocation interface and change types

--- src/contexts/GetLocation.tsx
+++ src/contexts/GetLocation.tsx
@@ -1,76 +1,75 @@
-import axios from 'axios'
-import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
-
+import axios from "axios";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { IGeolocation } from "../interfaces/IGeolocation";
 
 interface GetLocationContextProps {
-  children: ReactNode
-}
-
-type LocationData = {
-  location: {
-    name: string
-    localtime: string
-  },
-  current: {
-    condition: {
-      icon: string
-      text: string
-    }
-  }
+  children: ReactNode;
 }
 
 interface GetLocationData {
-  location: LocationData | undefined,
-  contextState: States
+  location: IGeolocation | undefined;
+  contextState: States;
 }
 
 export enum States {
   none,
   loading = "loading",
   completed = "completed",
-  error = "error"
+  error = "error",
 }
 
+export const GetLocationContext = createContext({} as GetLocationData);
 
-export const GetLocationContext = createContext({} as GetLocationData)
-
-export function GetLocationContextProvider({ children }: GetLocationContextProps) {
-  const [location, setLocation] = useState<LocationData>()
-  const [contextState, setContextState] = useState(States.none)
-
-
+export function GetLocationContextProvider({
+  children,
+}: GetLocationContextProps) {
+  const [location, setLocation] = useState<IGeolocation>();
+  const [contextState, setContextState] = useState(States.none);
 
   useEffect(() => {
-    setContextState(States.loading)
+    setContextState(States.loading);
 
     setTimeout(() => {
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-          await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=661026bef3784058b4b161650220602&q=${coords.latitude},${coords.longitude}&days=5&aqi=yes&alerts=yes`)
-            .then(response => setLocation(response.data))
-            setContextState(States.completed)
-        }, async function (err) {
-          await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=661026bef3784058b4b161650220602&q=paris&days=5&aqi=yes&alerts=yes`)
-            .then(response => setLocation(response.data))
-            setContextState(States.completed)
-        })
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          async ({ coords }) => {
+            await axios
+              .get(
+                `https://api.weatherapi.com/v1/forecast.json?key=661026bef3784058b4b161650220602&q=${coords.latitude},${coords.longitude}&days=5&aqi=yes&alerts=yes`
+              )
+              .then((response) => setLocation(response.data));
+            setContextState(States.completed);
+          },
+          async function (err) {
+            await axios
+              .get(
+                `https://api.weatherapi.com/v1/forecast.json?key=661026bef3784058b4b161650220602&q=paris&days=5&aqi=yes&alerts=yes`
+              )
+              .then((response) => setLocation(response.data));
+            setContextState(States.completed);
+          }
+        );
       }
     }, 5000);
-    
-   
-  }, [])
+  }, []);
   return (
-    <GetLocationContext.Provider value={{
-      location,
-      contextState
-
-    }}>
+    <GetLocationContext.Provider
+      value={{
+        location,
+        contextState,
+      }}
+    >
       {children}
     </GetLocationContext.Provider>
-  )
+  );
 }
-
 
 export const useGetLocationContext = () => {
-  return useContext(GetLocationContext)
-}
+  return useContext(GetLocationContext);
+};
