51c6d80f080a23a4d8f5101d459c750e818bfde0
mathmorais <gm80648@gmail.com>
2022-02-10 16:21:36 -0300

    Adjust WeatherCard resposiveness

--- src/components/WeatherCard/styles.ts
+++ src/components/WeatherCard/styles.ts
@@ -5,7 +5,7 @@ export const WeatherCardTemp = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  font-size: 1.1vw;
+  font-size: 1.1rem;
   margin-top: 2px;
   padding: 0 20px;
   p:last-of-type {
