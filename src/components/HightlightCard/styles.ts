b46e1070ef0f652af55eb3ef109fb9f464c594d7
mathmorais <gm80648@gmail.com>
2022-02-10 16:22:18 -0300

    Adjust Hightlights resposiveness

--- src/components/HightlightCard/styles.ts
+++ src/components/HightlightCard/styles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const HightlightCardText = styled.h1`
   font-size: 6rem;
   font-weight: 700;
-  margin-bottom: 25px;
 
   small {
     font-size: 2.5rem;
@@ -16,5 +15,6 @@ export const HightlightCardFooter = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 20px;
   height: 30px;
 `;
--- src/components/HightlightList/styles.ts
+++ src/components/HightlightList/styles.ts
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const HightlightListContainer = styled.div`
-  height: 70%;
+  height: 100%;
   & > h1 {
     font-size: 2rem;
     margin-bottom: 25px;
+  }
+  @media (max-width: 1200px) {
+    height: 75%;
   }
 `;
 
@@ -12,7 +15,7 @@ export const HightlightList = styled.section`
   display: grid;
   grid-template-columns: repeat(2, minmax(300px, 1fr));
   grid-template-rows: 1fr 0.75fr;
-  height: 90%;
+  height: 100%;
   width: 100%;
   gap: 48px;
   @media (max-width: 1200px) {
