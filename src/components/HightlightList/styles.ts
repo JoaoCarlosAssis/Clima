fa7b5a8fb4b73cb24fab966ec9ccc7e27f9a7c62
mathmorais <gm80648@gmail.com>
2022-02-11 15:59:53 -0300

    Improve resposiveness from Hightlight components

--- src/components/HightlightList/styles.ts
+++ src/components/HightlightList/styles.ts
@@ -1,13 +1,9 @@
 import styled from "styled-components";
 
 export const HightlightListContainer = styled.div`
-  height: 100%;
   & > h1 {
     font-size: 2rem;
     margin-bottom: 25px;
-  }
-  @media (max-width: 1200px) {
-    height: 75%;
   }
 `;
 
@@ -15,7 +11,6 @@ export const HightlightList = styled.section`
   display: grid;
   grid-template-columns: repeat(2, minmax(300px, 1fr));
   grid-template-rows: 1fr 0.75fr;
-  height: 100%;
   width: 100%;
   gap: 48px;
   @media (max-width: 1200px) {
