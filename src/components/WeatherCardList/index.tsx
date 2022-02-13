22b7272ae31e8496d0e0d2dd80a51f160064b3d5
mathmorais <gm80648@gmail.com>
2022-02-12 23:55:33 -0300

    Implement different units inside WeatherCard

--- src/components/WeatherCardList/index.tsx
+++ src/components/WeatherCardList/index.tsx
@@ -1,16 +1,22 @@
+import { useContext } from "react";
 import { useGetLocationContext } from "../../contexts/GetLocation";
+import { UnitContext } from "../../contexts/UnitContext";
 import { WeatherCard } from "../WeatherCard";
 import { WeatherList } from "./styles";
 
 export const WeatherCardList = () => {
   const { location } = useGetLocationContext();
+  const { handleGetMeasure } = useContext(UnitContext);
+
+  const currentMeasures = handleGetMeasure();
 
   const handleRenderWeatherCards = () => {
     return location?.forecast.forecastday.map((info, index) => (
       <WeatherCard
         key={index}
-        minTemp={info.day.mintemp_c}
-        maxTemp={info.day.maxtemp_c}
+        minTemp={`${info.day[`mintemp_${currentMeasures.temperature}`]}`}
+        maxTemp={`${info.day[`maxtemp_${currentMeasures.temperature}`]}`}
+        measure={currentMeasures.temperature}
         epoch={info.date_epoch}
         imageUrl={info.day.condition.icon}
       />
