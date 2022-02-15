52e3eb8055e3534a3e46dd6d34b02b4820ea69b9
mathmorais <gm80648@gmail.com>
2022-02-15 12:41:15 -0300

    Change map property

--- src/components/InputSelect/styles.ts
+++ src/components/InputSelect/styles.ts
@@ -1,27 +1,29 @@
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
 
-
 export const InputSelectContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 2rem;
+  margin-top: 25px;
 `;
 
 export const InputItem = styled.div`
+  display: flex;
+  align-items: center;
   color: ${colors.lightGrey};
   padding: 1rem 0.8rem;
-  
+  border: 1px solid transparent;
+
   &:hover {
-    border: 1px solid ${colors.white};
+    border-color: ${colors.white};
     cursor: pointer;
     color: ${colors.white};
     display: flex;
     justify-content: space-between;
     &::after {
-      content: '>';
+      content: ">";
     }
-    
   }
 `;
